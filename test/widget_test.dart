ab410f9605fc1a46563e64d366d6e844d7b704d8
Swanav <swanav@outlook.com>
2018-06-10 18:22:00 +0530

    Testing framework.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,26 +4,29 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:abstergo_flutter/Abstergo.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Login Page encountered', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(Abstergo());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Roll Number'), findsOneWidget);
+    expect(find.text('Password'), findsOneWidget);
+    expect(find.text('Submit'), findsOneWidget);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // await tester.enterText(find.text('Roll Number'), '101504122');
+    // await tester.enterText(find.text('Password'), '2405');
+    // await tester.tap(find.text('Submit'));
+    // await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // expect(find.text('Login'), findsNothing);
+    // expect(find.text('1'), findsOneWidget);
   });
 }
