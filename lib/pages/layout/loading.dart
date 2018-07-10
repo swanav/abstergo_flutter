138784739c11ed8b26c9f83a94eaae14d7789e80
Swanav <swanav@outlook.com>
2018-07-11 02:10:13 +0530

    Customizable color of loading spinner.

--- lib/pages/layout/loading.dart
+++ lib/pages/layout/loading.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class Loading extends StatelessWidget {
+
+  final Color color;
+
+  Loading({this.color = Colors.black});
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: CircularProgressIndicator(
-        valueColor: AlwaysStoppedAnimation<Color>(Colors.purple),
+        valueColor: AlwaysStoppedAnimation<Color>(color),
       ),
     );
   }
